42ac0976bcd82801cb2c81bfa25c15b563c57955
BuildTools <unconfigured@null.spigotmc.org>
2023-12-01 23:18:44 +0800

    Sellora - Mid-term assesment ready (Discussion needed on navbar situation)

--- Sellora/Server/Models/ApplicationUser.cs
+++ Sellora/Server/Models/ApplicationUser.cs
@@ -4,5 +4,7 @@ namespace Sellora.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
     }
 }
--- Sellora/Server/Program.cs
+++ Sellora/Server/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Sellora.Server.Data;
 using Sellora.Server.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddIdentityServer()
