51474cf0b55932a6b41d93de937943023dbedfee
int-RyanChong <2102197G@student.tp.edu.sg>
2024-01-16 13:34:49 +0800

    Sellora - Seeding Part 2

--- Sellora/Server/Data/ApplicationDbContext.cs
+++ Sellora/Server/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Sellora.Server.Configurations.Entities;
 using Sellora.Server.Models;
 using Sellora.Shared.Domain;
 
@@ -18,11 +19,18 @@ namespace Sellora.Server.Data
         public DbSet<User> Users {  get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<Staff> Staff { get; set; }
+
         public DbSet<Report> Reports { get; set; }
         public DbSet<SaleTransaction> SaleTransactions { get; set; }
         public DbSet<SwapTransaction> SwapTransactions { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<Category> Categories { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new StaffSeedConfiguration());
+        }
     }
 }
