34fde1506cdaa023461a97999fb9f3dcdfdd7d1f
LucasDo <cuongbao2011@gmail.com>
2024-01-16 14:09:51 +0800

    Sellora - CategorySeedConfig

---
+++ Sellora/Server/Configurations/Entities/CategorySeedConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sellora.Shared.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Drawing;
+
+namespace Sellora.Server.Configurations.Entities
+{
+    public class CategorySeedConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+            new Category
+            {
+                Id = 1,
+                CategoryName = "Technology",
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                CreatedBy = "System",
+                UpdatedBy = "System"
+            },
+            new Category
+            {
+                Id = 2,
+                CategoryName = "Fashion",
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                CreatedBy = "System",
+                UpdatedBy = "System"
+
+
+            }
+        );
+        }
+    }
+}
